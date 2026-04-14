cb7ea34c0bf6ecd718e79a19c156fcc4221e2c4d
michael39mm <125750317+michael39mm@users.noreply.github.com>
2026-04-14 18:16:41 +0200

    Update system_calls.h

--- Project/Project/include/system_calls.h
+++ Project/Project/include/system_calls.h
@@ -1,9 +1,18 @@
 #ifndef SYSTEM_CALLS_H
 #define SYSTEM_CALLS_H
 
-#include "os_core.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/memory.h"
 
-// System calls act as the bridge between the process and the OS resources
-void perform_system_call(const char* type, const char* arg1, const char* arg2);
+#define MAX_VALUE_LENGTH 256
+
+void syscall_print(int processId, char* variableName, MemoryWord* memory, int lowerBound, int upperBound);
+char* syscall_take_input(int processId);
+char* syscall_read_file(int processId, char* filename);
+void syscall_write_file(int processId, char* filename, char* data);
+char* syscall_read_memory(int processId, char* variableName, MemoryWord* memory, int lowerBound, int upperBound);
+void syscall_write_memory(int processId, char* variableName, char* value, MemoryWord* memory, int lowerBound, int upperBound);
 
 #endif
